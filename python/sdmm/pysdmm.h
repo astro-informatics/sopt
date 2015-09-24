fe78f04f52bcf40419f8af053fba2127c9b4440a
sinanshi <shisinancn@gmail.com>
2015-09-24 10:53:55 +0100

    sdmm python binding without callback functions

--- python/sdmm/pysdmm.h
+++ python/sdmm/pysdmm.h
@@ -2,36 +2,36 @@
 #define SOPT_PYTHON
 #include <sopt/types.h>
 #include <sopt/sdmm.h>
-
-
-
-
 namespace sopt { namespace pySDMM {
 
-  //template<class SCALAR>
   template<class T>
-    void c_sdmm_solver(T *input, T *result, const int N){
-
+    Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic>* proxdata2ptr(T *L_n, 
+        const int nrow, const int ncol, const int NPROX){
+        typedef Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic> t_Matrix;
+        t_Matrix *ptr = new t_Matrix [NPROX];
+        for(int i(0); i < NPROX; ++i)
+          ptr[i] = Eigen::Map<t_Matrix>(&L_n[nrow * ncol * i], nrow, ncol);
+      return(ptr);
+    }
+  template<class T>
+    void c_sdmm_solver(T *input, T *result, T *L_n, T *Tar_n, const int N, const int NPROX){
       sopt::logging::initialize();
       sopt::logging::set_level(SOPT_TEST_DEBUG_LEVEL);
-
-      // Some typedefs for simplicity
       typedef Eigen::Matrix<T, Eigen::Dynamic, 1> t_Vector;
       typedef Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic> t_Matrix;
 
-      t_Matrix const L0 = t_Matrix::Random(N, N) * 2;
-      t_Matrix const L1 = t_Matrix::Random(N, N) * 4;
-      // Creates the target vectors
-      t_Vector const target0 = t_Vector::Random(N);
-      t_Vector const target1 = t_Vector::Random(N);
+      auto ptr_ln = proxdata2ptr(L_n, N, N, NPROX);
+      auto ptr_target = proxdata2ptr(Tar_n, N, 1, NPROX);
 
       // Creates the resulting proximal
       // In practice g_0 and g_1 are any functions with the signature
       // void(t_Vector &output, t_Vector::Scalar gamma, t_Vector const &input)
-      auto prox_g0 = sopt::proximal::translate(sopt::proximal::EuclidianNorm(), -target0);
-      auto prox_g1 = sopt::proximal::translate(sopt::proximal::EuclidianNorm(), -target1);
+      auto prox_g0 = sopt::proximal::translate(sopt::proximal::EuclidianNorm(), -ptr_target[0]);
+      auto prox_g1 = sopt::proximal::translate(sopt::proximal::EuclidianNorm(), -ptr_target[1]);
 
-      typedef sopt::algorithm::SDMM<Eigen::Matrix<std::complex<double>, Eigen::Dynamic,1>::Scalar> SDMM;
+      typedef sopt::algorithm::SDMM<Eigen::Matrix<double, Eigen::Dynamic,1>::Scalar> SDMM;
+
+      
       std::shared_ptr<t_Vector> previous;
       auto relative = [&previous](SDMM const&, t_Vector const &candidate) {
         if(not previous) {
@@ -45,13 +45,14 @@ namespace sopt { namespace pySDMM {
         return result;
       };
 
+      
       auto sdmm = SDMM()
         .itermax(500) // maximum number of iterations
         .gamma(1)
         .conjugate_gradient(std::numeric_limits<sopt::t_uint>::max(), 1e-12)
         .is_converged(relative)
-        .append(prox_g0, L0)
-        .append(prox_g1, L1);
+        .append(prox_g0, ptr_ln[0])
+        .append(prox_g1, ptr_ln[1]);
 
       t_Vector c_result;
       auto c_input = Eigen::Map<t_Vector>(&input[0], N);
--- python/sdmm/sdmm.pyx
+++ python/sdmm/sdmm.pyx
@@ -3,19 +3,21 @@ from cython cimport view
 from libcpp.string cimport string
 
 cdef extern from "pysdmm.h" namespace "sopt::pySDMM":
-    void c_sdmm_solver[T](T* input, T* result, const int N) except+;
+    void c_sdmm_solver[T](T* input, T* result, T* ln_data, T* target_data, 
+                          int N, int NPROX) except+;
 
 
 class solver:
     def __init__(self, itermax = None, gamma = None, proximal = None, 
-                 Ln = None, cg_tolerance = 1e-12):
+                 Ln = None, targets = None, cg_tolerance = 1e-12):
         self.config = {}
-        self.paramlist = ['itermax', 'gamma', 'proximal', 'Ln', 'cg_tolerance']
+        self.paramlist = ['itermax', 'gamma', 'proximal', 'Ln', 'cg_tolerance','targets']
         self.config['itermax'] = itermax
         self.config['gamma'] = gamma
         self.config['cg_tolerance'] = cg_tolerance
         self.config['proximal'] = proximal
         self.config['Ln'] = Ln
+        self.config['targets'] = targets
     
     def __setitem__(self, key, item):
         if key not in self.config:
@@ -29,15 +31,28 @@ class solver:
 
 
     def solve(self, input):
-        N = input.size# need to be checked here
+        N = input.size # size need to be controled here
+        NPROX = len(self.config['Ln'])
+        
+        self.config['Ln'] = np.array(self.config['Ln'])
+        Ln = self.config['Ln'].reshape(self.config['Ln'].size)
+        
+        self.config['targets'] = np.array(self.config['targets'])
+        targets = self.config['targets'].reshape(self.config['targets'].size)
+        
         result = np.zeros(N, dtype=input.dtype)
         cdef:
             long input_data = input.ctypes.data
             long result_data = result.ctypes.data
-        c_sdmm_solver[complex](<double complex*>input_data, <double complex *>result_data, N)
-#        if input.dtype == 'float64':
-#            c_sdmm_solver[double](<double *>input_data, <double *>result_data, N)
+            long ln_data = Ln.ctypes.data
+            long target_data = Ln.ctypes.data
+
+        c_sdmm_solver[double](<double*>input_data, <double *>result_data,<double*>ln_data,
+                              <double*>target_data, <int>N, <int>NPROX)
+
+
 #        elif input.dtype == 'complex128':
+#        c_sdmm_solver[complex](<double complex*>input_data, <double complex *>result_data, N)
 
         print 'ok'
         return result
