ee8d03c4ec8293eda4bf8b20a5d08fd844588c70
rafael-carrillo <Rafael@Rafaels-MacBook-Air.local>
2016-09-27 10:34:11 +0200

    Revert "Merge branch 'master' into matlab"
    This reverts commit 922ba2e66b59a7c606e7b32de0dc2e531574378a, reversing
    changes made to 057b94b325f5269378b7569bd5867d2979a4734b.

--- cmake_files/compilations.cmake
+++
@@ -1,2 +0,0 @@
-try_compile(SOPT_HAS_USING "${CMAKE_CURRENT_BINARY_DIR}"
-  "${PROJECT_SOURCE_DIR}/cmake_files/using.cc")
--- cmake_files/export_sopt.cmake
+++ cmake_files/export_sopt.cmake
@@ -1,5 +1,6 @@
 # Exports Sopt so other packages can access it
-export(TARGETS sopt FILE "${PROJECT_BINARY_DIR}/SoptCPPTargets.cmake")
+export(TARGETS sopt_about libsopt
+    FILE "${PROJECT_BINARY_DIR}/SoptTargets.cmake")
 
 # Avoids creating an entry in the cmake registry.
 if(NOT NOEXPORT)
@@ -7,7 +8,10 @@ if(NOT NOEXPORT)
 endif()
 
 # First in binary dir
-set(ALL_INCLUDE_DIRS "${PROJECT_SOURCE_DIR}/cpp" "${PROJECT_BINARY_DIR}/include")
+set(ALL_INCLUDE_DIRS 
+    "${PROJECT_SOURCE_DIR}/include"
+    "${PROJECT_BINARY_DIR}/include"
+)
 configure_File(cmake_files/SoptConfig.in.cmake
     "${PROJECT_BINARY_DIR}/SoptConfig.cmake" @ONLY
 )
@@ -18,7 +22,7 @@ configure_File(cmake_files/SoptConfigVersion.in.cmake
 # Then for installation tree
 file(RELATIVE_PATH REL_INCLUDE_DIR
     "${CMAKE_INSTALL_PREFIX}/share/cmake/sopt"
-    "${CMAKE_INSTALL_PREFIX}/include"
+    "${CMAKE_INSTALL_PREFIX}/include/sopt"
 )
 set(ALL_INCLUDE_DIRS "\${Sopt_CMAKE_DIR}/${REL_INCLUDE_DIR}")
 configure_file(cmake_files/SoptConfig.in.cmake
@@ -33,4 +37,8 @@ install(FILES
     COMPONENT dev
 )
 
-install(EXPORT SoptCPPTargets DESTINATION share/cmake/sopt COMPONENT dev)
+install(EXPORT SoptTargets
+    DESTINATION share/cmake/sopt
+    COMPONENT dev
+)
+
