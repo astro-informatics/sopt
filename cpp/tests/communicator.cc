9c2f9590369c4aa38e33e793434855ddfd2f73ab
Mayeul d'Avezac <m.davezac@ucl.ac.uk>
2016-10-31 19:55:36 +0200

    Adding mpi to sopt

---
+++ cpp/tests/communicator.cc
@@ -0,0 +1,31 @@
+#include <iostream>
+#include "catch.hpp"
+
+#include "sopt/config.h"
+#include "sopt/communicator.h"
+
+using namespace sopt;
+
+#ifdef SOPT_MPI
+TEST_CASE("Creates an mpi communicator") {
+  int rank, size;
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+  mpi::Communicator const world;
+
+  SECTION("General stuff") {
+    REQUIRE(*world == MPI_COMM_WORLD);
+    REQUIRE(static_cast<t_int>(world.rank()) == rank);
+    REQUIRE(static_cast<t_int>(world.size()) == size);
+
+    mpi::Communicator shallow = world;
+    CHECK(*shallow == *world);
+  }
+
+  SECTION("Duplicate") {
+    mpi::Communicator dup = world.duplicate();
+    CHECK(*dup != *world);
+  }
+}
+#endif
