d5fd4873e70c019353f7e6285e95e38650c9c427
Mayeul d'Avezac <m.davezac@ucl.ac.uk>
2016-11-03 12:20:05 +0200

    Makes it easier to access sampling internals
    So we can broadcast them to other procs

--- cpp/sopt/sampling.h
+++ cpp/sopt/sampling.h
@@ -17,7 +17,7 @@ namespace sopt {
 class Sampling {
 public:
   //! Constructs from a vector
-  Sampling(t_uint size, std::vector<t_uint> const &indices) : indices(indices), size(size) {}
+  Sampling(t_uint size, std::vector<t_uint> const &indices) : indices_(indices), size(size) {}
   //! Constructs from the size and the number of samples to pick
   template <class RNG> Sampling(t_uint size, t_uint samples, RNG &&rng);
   //! Constructs from the size and the number of samples to pick
@@ -44,32 +44,35 @@ public:
   //! Size of the vector returned by the adjoint operation
   t_uint cols() const { return size; }
   //! Number of measurements
-  t_uint rows() const { return indices.size(); }
+  t_uint rows() const { return indices_.size(); }
+
+  //! Indices of sampled points
+  std::vector<t_uint> const & indices() const { return indices_; }
 
 protected:
   //! Set of indices to pick
-  std::vector<t_uint> indices;
+  std::vector<t_uint> indices_;
   //! Original vector size
   t_uint size;
 };
 
 template <class T0, class T1>
 void Sampling::operator()(Eigen::DenseBase<T0> &out, Eigen::DenseBase<T1> const &x) const {
-  out.resize(indices.size());
-  for(decltype(indices.size()) i(0); i < indices.size(); ++i) {
-    assert(indices[i] < static_cast<t_uint>(x.size()));
-    out[i] = x[indices[i]];
+  out.resize(indices_.size());
+  for(decltype(indices_.size()) i(0); i < indices_.size(); ++i) {
+    assert(indices_[i] < static_cast<t_uint>(x.size()));
+    out[i] = x[indices_[i]];
   }
 }
 
 template <class T0, class T1>
 void Sampling::adjoint(Eigen::DenseBase<T0> &out, Eigen::DenseBase<T1> const &x) const {
-  assert(static_cast<t_uint>(x.size()) == indices.size());
+  assert(static_cast<t_uint>(x.size()) == indices_.size());
   out.resize(out.size());
   out.fill(0);
-  for(decltype(indices.size()) i(0); i < indices.size(); ++i) {
-    assert(indices[i] < static_cast<t_uint>(out.size()));
-    out[indices[i]] = x[i];
+  for(decltype(indices_.size()) i(0); i < indices_.size(); ++i) {
+    assert(indices_[i] < static_cast<t_uint>(out.size()));
+    out[indices_[i]] = x[i];
   }
 }
 
@@ -83,10 +86,10 @@ template <class T> LinearTransform<Vector<T>> linear_transform(Sampling const &s
 }
 
 template <class RNG>
-Sampling::Sampling(t_uint size, t_uint samples, RNG &&rng) : indices(size), size(size) {
-  std::iota(indices.begin(), indices.end(), 0);
-  std::shuffle(indices.begin(), indices.end(), rng);
-  indices.resize(samples);
+Sampling::Sampling(t_uint size, t_uint samples, RNG &&rng) : indices_(size), size(size) {
+  std::iota(indices_.begin(), indices_.end(), 0);
+  std::shuffle(indices_.begin(), indices_.end(), rng);
+  indices_.resize(samples);
 }
 
 } /* sopt  */
