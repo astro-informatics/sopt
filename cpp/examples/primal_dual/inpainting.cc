f925222448d8b672d3cf0022bd57097613443e03
Adrian Jackson <adrianj@epcc.ed.ac.uk>
2016-11-03 23:30:39 +0000

    Adding initial primal dual implementation

---
+++ cpp/examples/primal_dual/inpainting.cc
@@ -0,0 +1,164 @@
+#include <algorithm>
+#include <exception>
+#include <functional>
+#include <iostream>
+#include <random>
+#include <vector>
+#include <Eigen/Eigenvalues>
+
+#include <sopt/primal_dual.h>
+#include <sopt/logging.h>
+#include <sopt/maths.h>
+#include <sopt/relative_variation.h>
+#include <sopt/sampling.h>
+#include <sopt/types.h>
+#include <sopt/utilities.h>
+#include <sopt/wavelets.h>
+#include <sopt/power_method.h>
+
+// This header is not part of the installed sopt interface
+// It is only present in tests
+#include <tools_for_tests/directories.h>
+#include <tools_for_tests/tiffwrappers.h>
+
+// \min_{x} ||\Psi^Tx||_1 \quad \mbox{s.t.} \quad ||y - Ax||_2 < \epsilon and x \geq 0
+int main(int argc, char const **argv) {
+  // Some typedefs for simplicity
+  typedef double Scalar;
+  // Column vector - linear algebra - A * x is a matrix-vector multiplication
+  // type expected by PrimalDual
+  typedef sopt::Vector<Scalar> Vector;
+
+  typedef sopt::Vector<sopt::t_complex> Complex;
+  // Matrix - linear algebra - A * x is a matrix-vector multiplication
+  // type expected by PrimalDual
+  typedef sopt::Matrix<Scalar> Matrix;
+  // Image - 2D array - A * x is a coefficient-wise multiplication
+  // Type expected by wavelets and image write/read functions
+  typedef sopt::Image<Scalar> Image;
+
+  
+  std::string const input = argc >= 2 ? argv[1] : "cameraman256";
+  std::string const output = argc == 3 ? argv[2] : "none";
+  if(argc > 3) {
+    std::cout << "Usage:\n"
+                 "$ "
+              << argv[0] << " [input [output]]\n\n"
+                            "- input: path to the image to clean (or name of standard SOPT image)\n"
+                            "- output: filename pattern for output image\n";
+    exit(0);
+  }
+  // Set up random numbers for C and C++
+  auto const seed = std::time(0);
+  std::srand((unsigned int)seed);
+  std::mt19937 mersenne(std::time(0));
+
+  // Initializes and sets logger (if compiled with logging)
+  // See set_level function for levels.
+  sopt::logging::initialize();
+
+  SOPT_HIGH_LOG("Read input file {}", input);
+  Image const image = sopt::notinstalled::read_standard_tiff(input);
+
+  SOPT_HIGH_LOG("Initializing sensing operator");
+  sopt::t_uint nmeasure = 0.33 * image.size();
+  auto const sampling
+      = sopt::linear_transform<Scalar>(sopt::Sampling(image.size(), nmeasure, mersenne));
+
+  SOPT_HIGH_LOG("Initializing wavelets");
+  auto const wavelet = sopt::wavelets::factory("DB4", 4);
+  auto const psi = sopt::linear_transform<Scalar>(wavelet, image.rows(), image.cols());
+
+  SOPT_HIGH_LOG("Computing primal-dual parameters");
+  Vector const y0 = sampling * Vector::Map(image.data(), image.size());
+  auto const snr = 30.0;
+  auto const sigma = y0.stableNorm() / std::sqrt(y0.size()) * std::pow(10.0, -(snr / 20.0));
+  auto const epsilon = std::sqrt(nmeasure + 2 * std::sqrt(y0.size())) * sigma;
+
+  SOPT_HIGH_LOG("Create dirty vector");
+  std::normal_distribution<> gaussian_dist(0, sigma);
+  Vector y(y0.size());
+  for(sopt::t_int i = 0; i < y0.size(); i++)
+    y(i) = y0(i) + gaussian_dist(mersenne);
+  // Write dirty image to file
+  if(output != "none") {
+    Vector const dirty = sampling.adjoint() * y;
+    sopt::utilities::write_tiff(Matrix::Map(dirty.data(), image.rows(), image.cols()),
+                                "dirty_" + output + ".tiff");
+  }
+
+  //  Vector rand = Vector::Random(image.size());
+  SOPT_HIGH_LOG("Setting up power method to calculate sigma values");
+  Eigen::EigenSolver<Matrix> es;
+  SOPT_HIGH_LOG("Setting up matrix A");
+
+  int N=10;
+  
+  Matrix A = Matrix::Random(N,N);
+  SOPT_HIGH_LOG("Setting up eigensolver");
+  es.compute(A.adjoint() * A, true);
+
+  auto const eigenvalues = es.eigenvalues();
+  auto const eigenvectors = es.eigenvectors();
+  Eigen::DenseIndex index;
+  (eigenvalues.transpose() * eigenvalues).real().maxCoeff(&index);
+  auto const eigenvalue = eigenvalues(index);
+  auto const eigenvector = eigenvectors.col(index);
+  // Create input vector close to solution
+  auto const rand = eigenvector * 1e-4 + Complex::Random(N);
+  auto const lt = sopt::linear_transform(A.cast<sopt::t_complex>());
+
+  auto const pm = sopt::algorithm::PowerMethod<sopt::t_complex>().tolerance(1e-12);
+
+  auto const tau = 0.49;
+  auto const kappa = 0.0001;
+
+  SOPT_HIGH_LOG("Calculating sigma1");
+  //auto const nu1 = pm.AtA(psi, rand);
+  auto const nu1data = pm.AtA(lt, rand);
+  nu1data.eigenvector.real().maxCoeff(&index);
+  auto const nu1 = nu1data.eigenvector(index).real();
+  //  auto const sigma1 = 1 / nu1*nu1;
+  auto const sigma1 = 1e0 / nu1*nu1;
+
+  SOPT_HIGH_LOG("Calculating sigma2");  
+  auto const nu2data = pm.AtA(lt, rand);
+  nu2data.eigenvector.real().maxCoeff(&index);
+  auto const nu2 = nu2data.eigenvector(index).real();
+  //  auto const nu2 = pm.AtA(sampling, rand);
+  //  auto const sigma2 = 1 / nu2*nu2;
+  auto const sigma2 = 1e0 / nu2*nu2;
+
+  SOPT_HIGH_LOG("Setting up primal dual algorithm");
+  
+  SOPT_HIGH_LOG("Creating primal-dual Functor");
+  auto const pd = sopt::algorithm::PrimalDual<Scalar>(y)
+                         .itermax(500)
+                         .tau(tau)
+                         .kappa(kappa)
+                         .sigma1(sigma1)
+                         .sigma2(sigma2)
+    // .l2ball_proximal_epsilon(epsilon)
+                         .nu(nu2*nu2)
+                         .Psi(psi)
+                         .Phi(sampling);
+
+  SOPT_HIGH_LOG("Starting primal dual");
+  // Alternatively, pd can be called with a tuple (x, residual) as argument
+  // Here, we default to (Φ^Ty/ν, ΦΦ^Ty/ν - y)
+  auto const diagnostic = pd();
+  SOPT_HIGH_LOG("primal dual returned {}", diagnostic.good);
+
+  // diagnostic should tell us the function converged
+  // it also contains diagnostic.niters - the number of iterations, and cg_diagnostic - the
+  // diagnostic from the last call to the conjugate gradient.
+  if(not diagnostic.good)
+    throw std::runtime_error("Did not converge!");
+
+  SOPT_HIGH_LOG("SOPT-primal-dual converged in {} iterations", diagnostic.niters);
+  if(output != "none")
+    sopt::utilities::write_tiff(Matrix::Map(diagnostic.x.data(), image.rows(), image.cols()),
+                                output + ".tiff");
+
+  return 0;
+}
---
+++ cpp/sopt/primal_dual.h
@@ -0,0 +1,259 @@
+#ifndef SOPT_PRIMAL_DUAL_H
+#define SOPT_PRIMAL_DUAL_H
+
+#include "sopt/config.h"
+#include <functional>
+#include <limits>
+#include "sopt/exception.h"
+#include "sopt/linear_transform.h"
+#include "sopt/logging.h"
+#include "sopt/types.h"
+#include "sopt/proximal.h"
+
+namespace sopt {
+namespace algorithm {
+
+//! \brief Primal Dual method
+//! \details This is a basic implementation of the primal dual method using a forward backwards algorithm.
+//! \f$\min_{x, y, z} f(x) + l(y) + h(z)\f$ subject to \f$Φx = y\f$, \f$Ψ^Hx = z\f$
+//!  We are not implementing blocking or parallelism here.
+template <class SCALAR> class PrimalDual {
+public:
+  //! Scalar type
+  typedef SCALAR value_type;
+  //! Scalar type
+  typedef value_type Scalar;
+  //! Real type
+  typedef typename real_type<Scalar>::type Real;
+  //! Type of then underlying vectors
+  typedef Vector<Scalar> t_Vector;
+  //! Type of the Ψ and Ψ^H operations, as well as Φ and Φ^H
+  typedef LinearTransform<t_Vector> t_LinearTransform;
+  //! Type of the convergence function
+  typedef ConvergenceFunction<Scalar> t_IsConverged;
+  //! Type of the convergence function
+  typedef ProximalFunction<Scalar> t_Proximal;
+
+  //! Values indicating how the algorithm ran
+  struct Diagnostic {
+    //! Number of iterations
+    t_uint niters;
+    //! Wether convergence was achieved
+    bool good;
+    //! the residual from the last iteration
+    t_Vector residual;
+
+    Diagnostic(t_uint niters = 0u, bool good = false)
+        : niters(niters), good(good), residual(t_Vector::Zero(0)) {}
+    Diagnostic(t_uint niters, bool good, t_Vector &&residual)
+        : niters(niters), good(good), residual(std::move(residual)) {}
+  };
+  //! Holds result vector as well
+  struct DiagnosticAndResult : public Diagnostic {
+    //! Output x
+    t_Vector x;
+  };
+
+  //! Setups PrimalDual
+  template <class DERIVED>
+  PrimalDual(Eigen::MatrixBase<DERIVED> const &target)
+    : itermax_(std::numeric_limits<t_uint>::max()), is_converged_(), kappa_(1), tau_(1), sigma1_(1),
+      sigma2_(1), nu_(1),
+      Phi_(linear_transform_identity<Scalar>()),
+      Psi_(linear_transform_identity<Scalar>()),
+      target_(target) {}
+  virtual ~PrimalDual() {}
+
+// Macro helps define properties that can be initialized as in
+// auto pd  = PrimalDual<float>().prop0(value).prop1(value);
+#define SOPT_MACRO(NAME, TYPE)                                                                     \
+  TYPE const &NAME() const { return NAME##_; }                                                     \
+  PrimalDual<SCALAR> &NAME(TYPE const &NAME) {                                                     \
+    NAME##_ = NAME;                                                                                \
+    return *this;                                                                                  \
+  }                                                                                                \
+                                                                                                   \
+protected:                                                                                         \
+  TYPE NAME##_;                                                                                    \
+                                                                                                   \
+public:
+
+  //! Maximum number of iterations
+  SOPT_MACRO(itermax, t_uint);
+    //! ν parameter
+  SOPT_MACRO(nu, Real);
+  //! κ
+  SOPT_MACRO(kappa, Real);
+  //! τ
+  SOPT_MACRO(tau, Real);
+  //! σ
+  SOPT_MACRO(sigma1, Real);
+  //! ς
+  SOPT_MACRO(sigma2, Real);
+  //! A function verifying convergence
+  SOPT_MACRO(is_converged, t_IsConverged);
+  //! Measurement operator
+  SOPT_MACRO(Phi, t_LinearTransform);
+  //! Analysis operator
+  SOPT_MACRO(Psi, t_LinearTransform);
+  
+#undef SOPT_MACRO
+
+  
+  //! Vector of target measurements
+  t_Vector const &target() const { return target_; }
+  //! Sets the vector of target measurements
+  template <class DERIVED> PrimalDual<DERIVED> &target(Eigen::MatrixBase<DERIVED> const &target) {
+    target_ = target;
+    return *this;
+  }
+
+    
+  
+  //! Facilitates call to user-provided convergence function
+  bool is_converged(t_Vector const &x) const {
+    return static_cast<bool>(is_converged()) and is_converged()(x);
+  }
+
+  
+  //! \brief Calls Primal Dual
+  //! \param[out] out: Output vector x
+  Diagnostic operator()(t_Vector &out) const { return operator()(out, initial_guess()); }
+  //! \brief Calls Primal Dual
+  //! \param[out] out: Output vector x
+  //! \param[in] guess: initial guess
+  Diagnostic operator()(t_Vector &out, std::tuple<t_Vector, t_Vector> const &guess) const {
+    return operator()(out, std::get<0>(guess), std::get<1>(guess));
+  }
+  //! \brief Calls Primal Dual
+  //! \param[in] guess: initial guess
+  DiagnosticAndResult operator()(std::tuple<t_Vector, t_Vector> const &guess) const {
+    DiagnosticAndResult result;
+    static_cast<Diagnostic &>(result) = operator()(result.x, guess);
+    return result;
+  }
+  //! \brief Calls Primal Dual
+  //! \param[in] guess: initial guess
+  DiagnosticAndResult operator()() const {
+    DiagnosticAndResult result;
+    static_cast<Diagnostic &>(result) = operator()(result.x, initial_guess());
+    return result;
+  }
+  //! Makes it simple to chain different calls to Primal Dual
+  DiagnosticAndResult operator()(DiagnosticAndResult const &warmstart) const {
+    DiagnosticAndResult result = warmstart;
+    static_cast<Diagnostic &>(result) = operator()(result.x, warmstart.x, warmstart.residual);
+    return result;
+  }
+  //! \brief Analysis operator Ψ and Ψ^† 
+  template <class... ARGS>
+  typename std::enable_if<sizeof...(ARGS) >= 1, PrimalDual &>::type Psi(ARGS &&... args) {
+    Psi_ = linear_transform(std::forward<ARGS>(args)...);
+    return *this;
+  }
+
+
+  //! Set Φ and Φ^† using arguments that sopt::linear_transform understands
+  template <class... ARGS>
+  typename std::enable_if<sizeof...(ARGS) >= 1, PrimalDual &>::type Phi(ARGS &&... args) {
+    Phi_ = linear_transform(std::forward<ARGS>(args)...);
+    return *this;
+  }
+  //! \brief Computes initial guess for x and the residual
+  //! \details with y the vector of measurements
+  //! - x = Φ^T y
+  //! - residuals = Φ x - y
+  std::tuple<t_Vector, t_Vector> initial_guess() const {
+    std::tuple<t_Vector, t_Vector> guess;
+    std::get<0>(guess) = Phi().adjoint() * target() / nu();
+    std::get<1>(guess) = Phi() * std::get<0>(guess) - target();    return guess;
+  }
+
+
+protected:
+
+  
+  void iteration_step(t_Vector &out, t_Vector &residual, t_Vector &s, t_Vector &v, t_Vector &x_bar) const;
+
+  //! Checks input makes sense
+  void sanity_check(t_Vector const &x_guess, t_Vector const &res_guess) const {
+    if((Phi().adjoint() * target()).size() != x_guess.size())
+      SOPT_THROW("target, adjoint measurement operator and input vector have inconsistent sizes");
+    if(target().size() != res_guess.size())
+      SOPT_THROW("target and residual vector have inconsistent sizes");
+    if((Phi() * x_guess).size() != target().size())
+      SOPT_THROW("target, measurement operator and input vector have inconsistent sizes");
+    if(not static_cast<bool>(is_converged()))
+      SOPT_WARN("No convergence function was provided: algorithm will run for {} steps", itermax());
+  }
+
+  //! \brief Calls Primal Dual
+  //! \param[out] out: Output vector x
+  //! \param[in] guess: initial guess
+  //! \param[in] residuals: initial residuals
+  Diagnostic operator()(t_Vector &out, t_Vector const &guess, t_Vector const &res) const;
+
+  //! Vector of measurements
+  t_Vector target_;
+};
+
+template <class SCALAR>
+void PrimalDual<SCALAR>::iteration_step(t_Vector &out, t_Vector &residual, t_Vector &s,
+					     t_Vector &v, t_Vector &x_bar) const {
+
+  t_Vector prev_sol = out;
+  t_Vector prev_s = s;
+  t_Vector prev_v = v;
+  t_Vector prev_x_bar = x_bar;
+
+  proximal::L2Ball<Real> l2ball_proximal = proximal::L2Ball<Real>(1.0);
+  
+  // v_t = v_t-1 + Phi*x_bar - l2ball_prox(v_t-1 + Phi*x_bar)
+  auto temp = v + Phi() * x_bar;
+  l2ball_proximal(v, temp);
+  v = temp - v;
+
+  // s_t = s_t-1 + Psi_dagger * x_bar_t-1 - l1norm_prox(s_t-1 + Psi_dagger * x_bar_t-1)
+  temp = s + Psi().adjoint() * x_bar;
+  proximal::l1_norm(s, kappa(), temp);
+  s = temp - s;
+  
+  //x_t = positive orth projection(x_t-1 - tau * (sigma1 * Psi * s + sigma2 * Phi dagger * v))
+  out = sopt::positive_quadrant(prev_sol - tau()*(Psi()*s*sigma1() + Phi().adjoint()*v*sigma2()));
+  x_bar = 2*out - prev_sol;
+  residual = Phi() * out - target();
+    
+}
+
+template <class SCALAR>
+typename PrimalDual<SCALAR>::Diagnostic PrimalDual<SCALAR>::
+operator()(t_Vector &out, t_Vector const &x_guess, t_Vector const &res_guess) const {
+  SOPT_HIGH_LOG("Performing Primal Dual");
+  sanity_check(x_guess, res_guess);
+
+  t_Vector s = t_Vector::Zero(target().size());
+  t_Vector v = t_Vector::Zero(target().size());
+  
+  t_Vector x_bar = t_Vector::Zero(target().size());
+  t_Vector residual = res_guess;
+
+  out = x_guess;
+
+  for(t_uint niters(0); niters < itermax(); ++niters) {
+    SOPT_LOW_LOG("    - Iteration {}/{}", niters, itermax());
+    iteration_step(out, residual, s, v, x_bar);
+    SOPT_LOW_LOG("      - Sum of residuals: {}", residual.array().abs().sum());
+
+    if(is_converged(out)) {
+      SOPT_MEDIUM_LOG("    - converged in {} of {} iterations", niters, itermax());
+      return {niters, true};
+    }
+  }
+  // check function exists, otherwise, don't know if convergence is meaningful
+  if(static_cast<bool>(is_converged()))
+    SOPT_ERROR("    - did not converge within {} iterations", itermax());
+  return {itermax(), false, std::move(residual)};
+}
+}
+} /* sopt::algorithm */
+#endif
