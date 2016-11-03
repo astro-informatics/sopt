dcbb58bab4a08d536b7f9891734ac9c78314ba9b
Mayeul d'Avezac <m.davezac@ucl.ac.uk>
2016-11-03 18:45:01 +0200

    Slight bugs in mpi l(1|2)_norm

--- cpp/sopt/mpi/utilities.h
+++ cpp/sopt/mpi/utilities.h
@@ -24,17 +24,17 @@ typename real_type<typename T0::Scalar>::type
 l2_norm(Eigen::ArrayBase<T0> const &input, Eigen::ArrayBase<T1> const &weights,
         Communicator const &comm) {
   if(weights.size() == 1) {
-    auto const weight_2 = weights(0) * std::conj(weights(0));
+    auto const weight_2 = std::real(weights(0) * std::conj(weights(0)));
     return std::sqrt(comm.all_sum_all(input.matrix().squaredNorm() * weight_2));
   }
-  return sopt::mpi::l2_norm((input * weights).matrix());
+  return sopt::mpi::l2_norm((input * weights).matrix(), comm);
 }
 //! Weighted l2-norm over distributed data
 template <class T0, class T1>
 typename real_type<typename T0::Scalar>::type
 l2_norm(Eigen::MatrixBase<T0> const &input, Eigen::MatrixBase<T1> const &weights,
         Communicator const &comm) {
-  return sopt::mpi::l2_norm(input.matrix(), weights.matrix(), comm);
+  return sopt::mpi::l2_norm(input.array(), weights.array(), comm);
 }
 
 //! Computes weighted L1 norm
