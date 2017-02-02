c3a6dc299b24c10bdf2781a8619788523fdd213a
Mayeul d'Avezac <m.davezac@ucl.ac.uk>
2017-02-02 16:49:31 +0000

    Update serial-vs-parallel for new convergence and ImagingProximalADMM

--- cpp/tests/serial_vs_parallel_padmm.cc
+++ cpp/tests/serial_vs_parallel_padmm.cc
@@ -107,24 +107,26 @@ TEST_CASE("Parallel vs serial inpainting") {
       },
       sampling.adjoint().sizes());
   padmm.Phi(parallel_sampling);
-  padmm.residual_convergence([&padmm, epsilon, split_comm, world](Vector const &residual) {
-    auto const tolerance = epsilon * 1.001;
-    auto const residual_norm
-        = sopt::mpi::l2_norm(residual, padmm.l2ball_proximal_weights(), split_comm);
-    SOPT_LOW_LOG("    - Residuals: epsilon = {}, residual norm = {}", tolerance, residual_norm);
-    auto const result = residual_norm < tolerance;
-    CHECK(result == (world.broadcast<int>(result, world.root_id()) != 0));
-    return result;
-  });
-  sopt::algorithm::ImagingProximalADMM<Scalar>::ObjectiveConvergence conv;
-  padmm.objective_convergence([&padmm, conv, split_comm,
-                               world](Vector const &residual) mutable -> bool {
-    auto const result = conv(padmm, sopt::mpi::l1_norm(residual + padmm.target(),
-                                                       padmm.l1_proximal_weights(), split_comm));
-    CHECK(result == (world.broadcast<int>(result, world.root_id()) != 0));
-    return result;
-  });
-  padmm.is_converged([world](Vector const &image) -> bool {
+  padmm.residual_convergence(
+      [&padmm, epsilon, split_comm, world](Vector const &, Vector const &residual) {
+        auto const tolerance = epsilon * 1.001;
+        auto const residual_norm
+            = sopt::mpi::l2_norm(residual, padmm.l2ball_proximal_weights(), split_comm);
+        SOPT_LOW_LOG("    - Residuals: epsilon = {}, residual norm = {}", tolerance, residual_norm);
+        auto const result = residual_norm < tolerance;
+        CHECK(result == (world.broadcast<int>(result, world.root_id()) != 0));
+        return result;
+      });
+  sopt::ScalarRelativeVariation<Scalar> conv(padmm.relative_variation(), padmm.relative_variation(),
+                                             "Objective function");
+  padmm.objective_convergence(
+      [&padmm, conv, split_comm, world](Vector const &, Vector const &residual) mutable -> bool {
+        auto const result = conv(
+            sopt::mpi::l1_norm(residual + padmm.target(), padmm.l1_proximal_weights(), split_comm));
+        CHECK(result == (world.broadcast<int>(result, world.root_id()) != 0));
+        return result;
+      });
+  padmm.is_converged([world](Vector const &image, Vector const &) -> bool {
     auto const from_root = world.broadcast(image);
     CHECK(from_root.isApprox(image, 1e-12));
     return true;
