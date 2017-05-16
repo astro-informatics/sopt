7f5cae40f87f92d819720e8d8cb2184ed4c1ded2
luke <Luke.Pratley.15@ucl.ac.uk>
2017-05-17 00:17:38 +0100

    Fixed input vector not matching size of adjoint in chained operator test.

--- cpp/tests/chained_operators.cc
+++ cpp/tests/chained_operators.cc
@@ -3,8 +3,8 @@
 #include <complex>
 #include <iomanip>
 
-#include "sopt/linear_transform.h"
 #include "sopt/chained_operators.h"
+#include "sopt/linear_transform.h"
 TEST_CASE("Linear Transforms", "[ops]") {
   using namespace sopt;
 
@@ -14,8 +14,10 @@ TEST_CASE("Linear Transforms", "[ops]") {
 
   SECTION("1 Functions") {
 
-    OperatorFunction<t_Vector> const func0 = [](t_Vector &out, t_Vector const &input) { out = input.array() * 2 - 1; };
-    OperatorFunction<t_Vector> const func1 = [](t_Vector &out, t_Vector const &input) { out = input.array() * 4 - 1; };
+    OperatorFunction<t_Vector> const func0
+        = [](t_Vector &out, t_Vector const &input) { out = input.array() * 2 - 1; };
+    OperatorFunction<t_Vector> const func1
+        = [](t_Vector &out, t_Vector const &input) { out = input.array() * 4 - 1; };
     t_Vector const x = t_Vector::Random(2 * N) * 5;
     auto chain = chained_operators(func0);
 
@@ -24,11 +26,13 @@ TEST_CASE("Linear Transforms", "[ops]") {
     chain(expected, x);
     CHECK(actual == expected);
   }
-  
+
   SECTION("2 Functions") {
 
-    OperatorFunction<t_Vector> const func0 = [](t_Vector &out, t_Vector const &input) { out = input.array() * 2 - 1; };
-    OperatorFunction<t_Vector> const func1 = [](t_Vector &out, t_Vector const &input) { out = input.array() * 4 - 1; };
+    OperatorFunction<t_Vector> const func0
+        = [](t_Vector &out, t_Vector const &input) { out = input.array() * 2 - 1; };
+    OperatorFunction<t_Vector> const func1
+        = [](t_Vector &out, t_Vector const &input) { out = input.array() * 4 - 1; };
     t_Vector const x = t_Vector::Random(2 * N) * 5;
     auto chain = chained_operators(func0, func1);
 
@@ -41,8 +45,10 @@ TEST_CASE("Linear Transforms", "[ops]") {
 
   SECTION("3 Functions") {
 
-    OperatorFunction<t_Vector> const func0 = [](t_Vector &out, t_Vector const &input) { out = input.array() * 2 - 1; };
-    OperatorFunction<t_Vector> const func1 = [](t_Vector &out, t_Vector const &input) { out = input.array() * 4 - 1; };
+    OperatorFunction<t_Vector> const func0
+        = [](t_Vector &out, t_Vector const &input) { out = input.array() * 2 - 1; };
+    OperatorFunction<t_Vector> const func1
+        = [](t_Vector &out, t_Vector const &input) { out = input.array() * 4 - 1; };
     t_Vector const x = t_Vector::Random(2 * N) * 5;
     auto chain = chained_operators(func0, func1, func0);
 
@@ -56,8 +62,10 @@ TEST_CASE("Linear Transforms", "[ops]") {
 
   SECTION("4 Functions") {
 
-    OperatorFunction<t_Vector> const func0 = [](t_Vector &out, t_Vector const &input) { out = input.array() * 2 - 1; };
-    OperatorFunction<t_Vector> const func1 = [](t_Vector &out, t_Vector const &input) { out = input.array() * 4 - 1; };
+    OperatorFunction<t_Vector> const func0
+        = [](t_Vector &out, t_Vector const &input) { out = input.array() * 2 - 1; };
+    OperatorFunction<t_Vector> const func1
+        = [](t_Vector &out, t_Vector const &input) { out = input.array() * 4 - 1; };
     t_Vector const x = t_Vector::Random(2 * N) * 5;
     auto chain = chained_operators(func0, func1, func0, func0);
 
@@ -72,10 +80,18 @@ TEST_CASE("Linear Transforms", "[ops]") {
 
   SECTION("linear transform") {
 
-    OperatorFunction<t_Vector> const func0 = [](t_Vector &out, t_Vector const &input) { out = input.head(N - 1).array() * 2; };
-    OperatorFunction<t_Vector> const func1 = [](t_Vector &out, t_Vector const &input) { out = input.head(N - 1).array() * 4; };
-    OperatorFunction<t_Vector> const afunc0 = [](t_Vector &out, t_Vector const &input) { out = t_Vector::Zero(N); out.head(N - 1) = input.array() * 2; };
-    OperatorFunction<t_Vector> const afunc1 = [](t_Vector &out, t_Vector const &input) { out = t_Vector::Zero(N); out.head(N - 1) = input.array() * 4; };
+    OperatorFunction<t_Vector> const func0
+        = [](t_Vector &out, t_Vector const &input) { out = input.head(N - 1).array() * 2; };
+    OperatorFunction<t_Vector> const func1
+        = [](t_Vector &out, t_Vector const &input) { out = input.head(N - 1).array() * 4; };
+    OperatorFunction<t_Vector> const afunc0 = [](t_Vector &out, t_Vector const &input) {
+      out = t_Vector::Zero(N);
+      out.head(N - 1) = input.head(N - 1).array() * 2;
+    };
+    OperatorFunction<t_Vector> const afunc1 = [](t_Vector &out, t_Vector const &input) {
+      out = t_Vector::Zero(N);
+      out.head(N - 1) = input.head(N - 1).array() * 4;
+    };
     t_Vector const x = t_Vector::Random(2 * N) * 5;
     auto chain = chained_operators(func0, func1, func0, func0);
     auto chain_adjoint = chained_operators(afunc0, afunc0, afunc1, afunc0);
@@ -83,10 +99,10 @@ TEST_CASE("Linear Transforms", "[ops]") {
     t_Vector actual, expected;
     chain(actual, x);
     expected = op * x;
+    CHECK(expected == x.head(N - 1) * 32);
     CHECK(actual == expected);
     chain_adjoint(actual, x.head(N - 1));
     expected = op.adjoint() * x.head(N - 1);
     CHECK(actual == expected);
   }
-
 }
