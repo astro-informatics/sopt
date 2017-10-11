6afdfab314d71d093938d5c31a8589dc06dba12d
luke <Luke.Pratley.15@ucl.ac.uk>
2017-10-11 06:04:09 +0100

    updated tests for credible_region and forward-backward

--- cpp/tests/credible_region.cc
+++ cpp/tests/credible_region.cc
@@ -1,5 +1,6 @@
 #include "sopt/credible_region.h"
 #include <iostream>
+#include "sopt/objective_functions.h"
 #include "sopt/types.h"
 #include "catch.hpp"
 
@@ -10,15 +11,18 @@ typedef Image<Scalar> t_Image;
 t_uint rows = 128;
 t_uint cols = 128;
 t_uint N = rows * cols;
-const t_real alpha = 0.01;
 
 TEST_CASE("calculating gamma") {
+  sopt::logging::set_level("debug");
   const std::function<t_real(t_Vector)> energy_function
       = [](const t_Vector &input) -> t_real { return 0.; };
   const t_Vector x = t_Vector::Random(N);
   CHECK(0 == energy_function(x));
-  const t_real gamma = credible_region::compute_energy_upper_bound(alpha, x, energy_function);
-  CHECK(gamma == N * (std::sqrt(16 * std::log(3 / (1 - alpha)) / N) + 1));
+  for(t_uint i = 1; i < 10; i++) {
+    const t_real alpha = 0.9 + i * 0.01;
+    const t_real gamma = credible_region::compute_energy_upper_bound(alpha, x, energy_function);
+    CHECK(gamma == N * (std::sqrt(16 * std::log(3 / (1 - alpha)) / N) + 1));
+  }
 }
 TEST_CASE("caculating upper and lower interval") {
   const t_Vector x = t_Vector::Constant(N, 0.5);
@@ -28,41 +32,45 @@ TEST_CASE("caculating upper and lower interval") {
   std::tuple<t_uint, t_uint, t_uint, t_uint> const region = {0, 0, rows, cols};
   t_real lower = 0;
   t_real upper = 0;
-  std::tie(lower, upper)
+  t_real mean = 0;
+  std::tie(lower, mean, upper)
       = credible_region::find_credible_interval(x, rows, cols, region, energy_function, gamma);
   CHECK(std::abs(lower + 1.5) <= 1e-4);
+  CHECK(std::abs(mean - 0.5) <= 1e-4);
   CHECK(std::abs(upper - 0.5) <= 1e-4);
-  std::tie(lower, upper)
+  std::tie(lower, mean, upper)
       = credible_region::find_credible_interval(x, rows, cols,
                                                 {std::floor(rows * 0.25), std::floor(cols * 0.25),
                                                  std::floor(rows * 0.5), std::floor(cols * 0.5)},
                                                 energy_function, gamma);
   CHECK(std::abs(lower + 1.5) <= 1e-4);
   CHECK(std::abs(upper - 0.5) <= 1e-4);
+  CHECK(std::abs(mean - 0.5) <= 1e-4);
 }
 
 TEST_CASE("calculating upper and lower interval grid") {
-  sopt::logging::set_level("debug");
   const t_uint pix_size = 16;
   const t_uint grid_cols = std::floor(cols / pix_size);
   const t_uint grid_rows = std::floor(rows / pix_size);
   const t_real gamma = 1.;
   t_Image image = t_Image::Constant(rows, cols, 0);
-  Image<t_real> expected_lower = Image<t_real>::Constant(grid_rows, grid_cols, -gamma);
-  Image<t_real> expected_upper = Image<t_real>::Constant(grid_rows, grid_cols, gamma);
+  const Image<t_real> expected_lower = Image<t_real>::Constant(grid_rows, grid_cols, -gamma);
+  const Image<t_real> expected_mean = Image<t_real>::Constant(grid_rows, grid_cols, 0);
+  const Image<t_real> expected_upper = Image<t_real>::Constant(grid_rows, grid_cols, gamma);
   const t_Vector x = t_Vector::Map(image.data(), image.size());
   const std::function<t_real(t_Vector)> energy_function
       = [&](const t_Vector &input) -> t_real { return input.cwiseAbs().maxCoeff(); };
   Image<t_real> lower = Image<t_real>::Zero(rows, cols);
+  Image<t_real> mean = Image<t_real>::Zero(rows, cols);
   Image<t_real> upper = Image<t_real>::Zero(rows, cols);
-  std::tie(lower, upper) = credible_region::credible_interval_grid<t_Vector, t_real>(
+  std::tie(lower, mean, upper) = credible_region::credible_interval_grid<t_Vector, t_real>(
       x, rows, cols, pix_size, energy_function, gamma);
   CHECK(expected_lower.isApprox(lower, 1e-4));
+  CHECK(expected_mean.isApprox(mean, 1e-4));
   CHECK(expected_upper.isApprox(upper, 1e-4));
 }
 
 TEST_CASE("calculating upper and lower interval grid non const") {
-  sopt::logging::set_level("debug");
   const t_uint pix_size = 16;
   rows = 145;
   cols = 153;
@@ -73,13 +81,16 @@ TEST_CASE("calculating upper and lower interval grid non const") {
   const t_Vector x = t_Vector::Map(image.data(), image.size());
   const std::function<t_real(t_Vector)> energy_function
       = [&](const t_Vector &input) -> t_real { return input.cwiseAbs().maxCoeff(); };
-  const t_real gamma = grid_cols * grid_rows;
+  const t_real gamma = 1.;
   Image<t_real> lower = Image<t_real>::Zero(rows, cols);
+  Image<t_real> mean = Image<t_real>::Zero(rows, cols);
   Image<t_real> upper = Image<t_real>::Zero(rows, cols);
-  std::tie(lower, upper) = credible_region::credible_interval_grid<t_Vector, t_real>(
+  std::tie(lower, mean, upper) = credible_region::credible_interval_grid<t_Vector, t_real>(
       x, rows, cols, pix_size, energy_function, gamma);
   Image<t_real> const expected_lower = Image<t_real>::Constant(grid_rows, grid_cols, -gamma);
+  Image<t_real> const expected_mean = Image<t_real>::Constant(grid_rows, grid_cols, 0);
   Image<t_real> const expected_upper = Image<t_real>::Constant(grid_rows, grid_cols, gamma);
   CHECK(expected_lower.isApprox(lower, 1e-4));
+  CHECK(expected_mean.isApprox(mean, 1e-4));
   CHECK(expected_upper.isApprox(upper, 1e-4));
 }
--- cpp/tests/forward_backward.cc
+++ cpp/tests/forward_backward.cc
@@ -1,13 +1,22 @@
 #include <catch.hpp>
 #include <random>
+#include <vector>
 
 #include <Eigen/Dense>
 
-#include "sopt/forward_backward.h"
-#include "sopt/imaging_forward_backward.h"
-#include "sopt/proximal.h"
-#include "sopt/types.h"
+#include <sopt/imaging_forward_backward.h>
+#include <sopt/logging.h>
+#include <sopt/maths.h>
+#include <sopt/relative_variation.h>
+#include <sopt/sampling.h>
+#include <sopt/types.h>
+#include <sopt/utilities.h>
+#include <sopt/wavelets.h>
 
+// This header is not part of the installed sopt interface
+// It is only present in tests
+#include <tools_for_tests/directories.h>
+#include <tools_for_tests/tiffwrappers.h>
 sopt::t_int random_integer(sopt::t_int min, sopt::t_int max) {
   extern std::unique_ptr<std::mt19937_64> mersenne;
   std::uniform_int_distribution<sopt::t_int> uniform_dist(min, max);
@@ -17,7 +26,10 @@ sopt::t_int random_integer(sopt::t_int min, sopt::t_int max) {
 typedef sopt::t_real Scalar;
 typedef sopt::Vector<Scalar> t_Vector;
 typedef sopt::Matrix<Scalar> t_Matrix;
-
+typedef sopt::t_real t_real;
+typedef sopt::t_uint t_uint;
+typedef sopt::t_int t_int;
+typedef sopt::Image<Scalar> t_Image;
 auto const N = 5;
 
 TEST_CASE("Forward Backward with ||x - x0||_2 function", "[fb][integration]") {
@@ -35,7 +47,36 @@ TEST_CASE("Forward Backward with ||x - x0||_2 function", "[fb][integration]") {
                       .beta(0.1);
   auto const result = fb();
 }
-
+TEST_CASE("Forward Backward imaging objective function") {
+  t_Vector const y = t_Vector::Random(N);
+  auto const phiTphi = [](t_Vector &out, t_Vector const &x) { out = x; };
+  auto const Psi = t_Matrix::Identity(N, N);
+  auto const fb = sopt::algorithm::ImagingForwardBackward<Scalar>(y)
+                      .itermax(500)
+                      .beta(1)
+                      .sigma(1.)
+                      .mu(0.01)
+                      .relative_variation(5e-4)
+                      .residual_tolerance(0)
+                      .tight_frame(false)
+                      .l1_proximal_tolerance(1e-4)
+                      .l1_proximal_nu(1)
+                      .l1_proximal_itermax(50)
+                      .l1_proximal_positivity_constraint(true)
+                      .l1_proximal_real_constraint(true)
+                      .Psi(Psi)
+                      .PhiTPhi(phiTphi);
+  t_Vector const input = t_Vector::Random(N);
+  const std::function<t_real(t_Vector)> objective_function = fb.objective_function();
+  auto const expected = [&](t_Vector const &x) {
+    return 0.01 * x.cwiseAbs().sum() + 0.5 * std::pow((y - x).norm(), 2);
+  };
+  auto const energy = objective_function(input);
+  auto const energy_expected = expected(input);
+  CAPTURE(energy);
+  CAPTURE(energy_expected);
+  CHECK(std::abs(energy - energy_expected) < 1e-6);
+}
 template <class T>
 struct is_imaging_proximal_ref
     : public std::is_same<sopt::algorithm::ImagingForwardBackward<double> &, T> {};
