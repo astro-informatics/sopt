07924011d163b3c3ccaf3c0b945207de174b82ab
Luke Pratley <Luke.Pratley.15@ucl.ac.uk>
2018-06-14 11:44:33 +0100

    Removed 3 infront of integer

--- cpp/sopt/mpi/communicator.cc
+++ cpp/sopt/mpi/communicator.cc
@@ -24,7 +24,7 @@ Communicator::Communicator(MPI_Comm const &comm) : impl(nullptr), session(sessio
 }
 
 void Communicator::abort(const std::string & reason) const{
-  fprintf(stderr, "MPI Error on Rank %3i: %s\n", rank(), reason.c_str());
+  fprintf(stderr, "MPI Error on Rank %i: %s\n", rank(), reason.c_str());
   MPI_Abort(**this, MPI_ERR_OTHER);
 }
 
