909a11e3176300f622e6bf1119deaa5f76e95df8
Luke Pratley <luke.pratley.15@ucl.ac.uk>
2018-08-10 18:56:28 +0100

    Added a test for all_to_allv

--- cpp/tests/communicator.cc
+++ cpp/tests/communicator.cc
@@ -101,13 +101,14 @@ TEST_CASE("Creates an mpi communicator") {
   }
 
   SECTION("Gather an std::vector") {
-    std::vector<t_int> const input{static_cast<t_int>(world.size()), static_cast<t_int>(world.rank())};
+    std::vector<t_int> const input{static_cast<t_int>(world.size()),
+                                   static_cast<t_int>(world.rank())};
     auto const result = world.gather(input, world.gather<t_int>(input.size()));
     if(world.is_root()) {
       CHECK(result.size() == world.size() * 2);
       for(decltype(world.size()) i(0); i < world.size(); ++i) {
         CHECK(result[2 * i] == world.size());
-        CHECK(result[2 *i + 1] == i);
+        CHECK(result[2 * i + 1] == i);
       }
     } else
       CHECK(result.size() == 0);
@@ -154,6 +155,23 @@ TEST_CASE("Creates an mpi communicator") {
       auto const expected = "Hello World!";
       std::string const input = world.is_root() ? expected : "";
       CHECK(world.broadcast(input) == expected);
+    }
+    SECTION("all_to_allv") {
+      //
+      std::vector<t_int> sizes(world.size(), world.rank());
+      const Vector<t_int> sendee
+          = Vector<t_int>::Constant(std::accumulate(sizes.begin(), sizes.end(), 0), world.rank());
+      const Vector<t_int> output = world.all_to_allv(sendee, sizes);
+      t_int sum = 0;
+      for(t_int i = 0; i < world.size() - 1; i++) {
+        const Vector<t_int> expected = Vector<t_int>::Constant(i + 1, i + 1);
+        CAPTURE(sum);
+        CAPTURE(i);
+        CAPTURE(output.segment(sum, i + 1));
+        CAPTURE(expected);
+        REQUIRE(output.segment(sum, i + 1).isApprox(expected));
+        sum += i + 1;
+      }
     }
   }
 }
