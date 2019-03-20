32dfd5cdd7afd1a66323a25f6d138bcce8ca1ab9
Luke Pratley <luke.pratley.15@ucl.ac.uk>
2019-03-20 14:22:22 +0000

    undo choosing of eigen version

--- cmake_files/dependencies.cmake
+++ cmake_files/dependencies.cmake
@@ -1,7 +1,6 @@
 include(PackageLookup)  # check for existence, or install external projects
 
-lookup_package(Eigen3 REQUIRED DOWNLOAD_BY_DEFAULT ARGUMENTS URL "http://bitbucket.org/eigen/eigen/get/3.3.7.tar.gz" MD5 "f2a417d083fe8ca4b8ed2bc613d20f07")
-
+lookup_package(Eigen3 REQUIRED)
 if(logging)
   lookup_package(spdlog REQUIRED)
 endif()
