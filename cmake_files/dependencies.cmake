ed9d06141a895a0bd2cf55c560275bb82d75ba93
mattgillucl <40172038+mattgillucl@users.noreply.github.com>
2019-08-22 11:19:08 +0100

    Updating Eigen3 version (#250)
    * Update Eigen3 to the new hash for the needed release.

--- cmake_files/dependencies.cmake
+++ cmake_files/dependencies.cmake
@@ -1,6 +1,6 @@
 include(PackageLookup)  # check for existence, or install external projects
 
-lookup_package(Eigen3 REQUIRED DOWNLOAD_BY_DEFAULT ARGUMENTS URL "http://bitbucket.org/eigen/eigen/get/3.2.tar.gz" MD5 "035ccc791f046f48e90bb1fb42ce227e")
+lookup_package(Eigen3 REQUIRED DOWNLOAD_BY_DEFAULT ARGUMENTS URL "http://bitbucket.org/eigen/eigen/get/3.2.tar.gz" MD5 "7b929312ebe77ae7d441e715a1ca7635")
 if(logging)
   lookup_package(spdlog REQUIRED)
 endif()
