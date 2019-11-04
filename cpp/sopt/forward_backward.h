42a0fa92a44a397defddb98ac2b211ef9df8a1da
Luke Pratley <luke.pratley.15@ucl.ac.uk>
2019-11-04 00:52:06 -0500

    changed how operator norm is appied inside algorithm

--- cpp/sopt/forward_backward.h
+++ cpp/sopt/forward_backward.h
@@ -233,7 +233,7 @@ void ForwardBackward<SCALAR>::iteration_step(t_Vector &out, t_Vector &residual, 
   f_gradient(z, residual);
   g_proximal(out, gamma() * beta(), out - beta() / nu() * (Phi().adjoint() * z));
   p = out + lambda * (out - p);
-  residual = (Phi() * p) / nu() - target();
+  residual = (Phi() * p) - target();
 }
 
 template <class SCALAR>
