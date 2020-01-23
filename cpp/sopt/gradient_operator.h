74119ad745ff0b00fef9897cded5158332b6a1c0
Luke Pratley <Luke.Pratley@gmail.com>
2020-01-23 23:28:48 +0100

    linting

--- cpp/sopt/gradient_operator.h
+++ cpp/sopt/gradient_operator.h
@@ -2,68 +2,66 @@
 #define SOPT_GRADIENT_OPERATOR_H
 
 #include "sopt/config.h"
-#include "sopt/logging.h"
 #include "sopt/linear_transform.h"
+#include "sopt/logging.h"
 
 namespace sopt {
 namespace gradient_operator {
-  //! Numerical derivative of 1d vector
-  template <class T>
-    Vector<T> diff(const Vector<T> & x){
-      if(x.size() < 2)
-        return Vector<T>::Zero(x.size());
-      Vector<T> output = Vector<T>::Zero(x.size());
-      output.segment(0, x.size() - 1) = x.segment(1, x.size() - 1) - x.segment(0, x.size() - 1);
-      return output;
-    }
-  //! Numerical derivative adjoint of 1d vector
-  template <class T>
-    Vector<T> diff_adjoint(const Vector<T> & x){
-      Vector<T> output = Vector<T>::Zero(x.size());
-      output.segment(0, x.size() - 1) -= x.segment(0, x.size() - 1);
-      if(output.size() > 2)
-       output.segment(1, x.size() - 1) = x.segment(0, x.size() - 1);
-      return output;
-    }
-  //! Numerical derivative of 2d image
-  template <class T>
-    Vector<T> diff2d(const Vector<T> & x, const t_int rows, const t_int cols){
-      Matrix<T> output = Matrix<T>::Zero(rows, 2*  cols);
-      const Matrix<T> & input_image = Matrix<T>::Map(x.data(), rows, cols);
-      for(Eigen::Index i(0); i < rows; i++)
-        output.block(0, 0, rows, cols).row(i) = diff<T>(input_image.row(i));
-      for(Eigen::Index i(0); i < cols; i++)
-        output.block(0, cols, rows, cols).col(i) = diff<T>(input_image.col(i));
-      return Vector<T>::Map(output.data(), output.size());
-    }
-  //! Numerical derivative adjoint of 2d image
-  template <class T>
-    Vector<T> diff2d_adjoint(const Vector<T> & x, const t_int rows, const t_int cols){
-      const Matrix<T> & input_image = Matrix<T>::Map(x.data(),  rows, 2 * cols);
-      Matrix<T> output = Matrix<T>::Zero(rows, cols);
-      for(Eigen::Index i(0); i < rows; i++)
-         output.row(i) += diff_adjoint<T>(input_image.block(0, 0, rows, cols).row(i));
-      for(Eigen::Index i(0); i < cols; i++)
-         output.col(i) += diff_adjoint<T>(input_image.block(0, cols, rows, cols).col(i));
-      return Vector<T>::Map(output.data(), output.size());
-    }
+//! Numerical derivative of 1d vector
+template <class T>
+Vector<T> diff(const Vector<T> &x) {
+  if (x.size() < 2) return Vector<T>::Zero(x.size());
+  Vector<T> output = Vector<T>::Zero(x.size());
+  output.segment(0, x.size() - 1) = x.segment(1, x.size() - 1) - x.segment(0, x.size() - 1);
+  return output;
+}
+//! Numerical derivative adjoint of 1d vector
+template <class T>
+Vector<T> diff_adjoint(const Vector<T> &x) {
+  Vector<T> output = Vector<T>::Zero(x.size());
+  output.segment(0, x.size() - 1) -= x.segment(0, x.size() - 1);
+  if (output.size() > 2) output.segment(1, x.size() - 1) = x.segment(0, x.size() - 1);
+  return output;
+}
+//! Numerical derivative of 2d image
+template <class T>
+Vector<T> diff2d(const Vector<T> &x, const t_int rows, const t_int cols) {
+  Matrix<T> output = Matrix<T>::Zero(rows, 2 * cols);
+  const Matrix<T> &input_image = Matrix<T>::Map(x.data(), rows, cols);
+  for (Eigen::Index i(0); i < rows; i++)
+    output.block(0, 0, rows, cols).row(i) = diff<T>(input_image.row(i));
+  for (Eigen::Index i(0); i < cols; i++)
+    output.block(0, cols, rows, cols).col(i) = diff<T>(input_image.col(i));
+  return Vector<T>::Map(output.data(), output.size());
+}
+//! Numerical derivative adjoint of 2d image
+template <class T>
+Vector<T> diff2d_adjoint(const Vector<T> &x, const t_int rows, const t_int cols) {
+  const Matrix<T> &input_image = Matrix<T>::Map(x.data(), rows, 2 * cols);
+  Matrix<T> output = Matrix<T>::Zero(rows, cols);
+  for (Eigen::Index i(0); i < rows; i++)
+    output.row(i) += diff_adjoint<T>(input_image.block(0, 0, rows, cols).row(i));
+  for (Eigen::Index i(0); i < cols; i++)
+    output.col(i) += diff_adjoint<T>(input_image.block(0, cols, rows, cols).col(i));
+  return Vector<T>::Map(output.data(), output.size());
+}
 template <class T>
 LinearTransform<Vector<T>> gradient_operator(const t_int rows, const t_int cols) {
   return LinearTransform<Vector<T>>(
       [rows, cols](Vector<T> &out, Vector<T> const &x) {
-      assert(x.size() = rows * cols * 2);
-      out = diff2d_adjoint(x, rows, cols) / 2.;
-      assert(out.size() == rows * cols);
+        assert(x.size() = rows * cols * 2);
+        out = diff2d_adjoint(x, rows, cols) / 2.;
+        assert(out.size() == rows * cols);
       },
       {{0, 1, static_cast<t_int>(rows * cols)}},
       [rows, cols](Vector<T> &out, Vector<T> const &x) {
-      assert(x.size() == rows * cols);
-     out = diff2d(x, rows, cols) / 2.;
-      assert(out.size() == 2 * rows * cols);
+        assert(x.size() == rows * cols);
+        out = diff2d(x, rows, cols) / 2.;
+        assert(out.size() == 2 * rows * cols);
       },
       {{0, 1, static_cast<t_int>(2 * rows * cols)}});
 }
-}
+}  // namespace gradient_operator
 }  // namespace sopt
 
 #endif
--- cpp/tests/gradient_operator.cc
+++ cpp/tests/gradient_operator.cc
@@ -3,8 +3,8 @@
 #include <random>
 
 #include <sopt/config.h>
-#include <sopt/types.h>
 #include <sopt/gradient_operator.h>
+#include <sopt/types.h>
 #include <sopt/utilities.h>
 
 // This header is not part of the installed sopt interface
@@ -22,28 +22,24 @@ TEST_CASE("Gradient Operator") {
   typedef sopt::Matrix<Scalar> Matrix;
 
   typedef sopt::Image<Scalar> Image;
-  
+
   Image const image = sopt::notinstalled::read_standard_tiff("cameraman256");
   auto const psi = sopt::gradient_operator::gradient_operator<Scalar>(image.rows(), image.cols());
   Matrix input = Matrix::Ones(image.rows(), image.cols());
-  for(Eigen::Index i(0); i < image.rows(); i++)
-    input.row(i) *= static_cast<Scalar>(i);
+  for (Eigen::Index i(0); i < image.rows(); i++) input.row(i) *= static_cast<Scalar>(i);
   Vector output = psi.adjoint() * Vector::Map(input.data(), input.size());
   CAPTURE(output.segment(0, 5));
   CAPTURE(output.segment(image.size(), 5));
   CHECK(output.size() == 2 * input.size());
   CHECK(output.segment(0, input.size()).isApprox(Vector::Zero(input.size())));
-  CHECK(output.segment(input.size(), input.size() - 1).isApprox(Vector::Constant(0.5, input.size() - 1)));
+  CHECK(output.segment(input.size(), input.size() - 1)
+            .isApprox(Vector::Constant(0.5, input.size() - 1)));
   input = Matrix::Ones(image.rows(), image.cols());
-  for(Eigen::Index i(0); i < image.cols(); i++)
-    input.col(i) *= static_cast<Scalar>(i);
+  for (Eigen::Index i(0); i < image.cols(); i++) input.col(i) *= static_cast<Scalar>(i);
   output = psi.adjoint() * Vector::Map(input.data(), input.size());
   CAPTURE(output.segment(0, 5));
   CAPTURE(output.segment(image.size(), 5));
   CHECK(output.size() == 2 * input.size());
   CHECK(output.segment(0, input.size() - 1).isApprox(Vector::Constant(0.5, input.size() - 1)));
   CHECK(output.segment(input.size(), input.size()).isApprox(Vector::Zero(input.size())));
-
-  
 }
-
