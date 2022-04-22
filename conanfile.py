476aefd0c8fd1eeaadd6496e241e595b01ffcf32
Tuomas Koskela <t.koskela@ucl.ac.uk>
2022-04-22 11:02:00 +0100

    Override version of zlib when doxygen or tiff are required

--- conanfile.py
+++ conanfile.py
@@ -21,10 +21,16 @@ class SoptConan(ConanFile):
                        "openmp": 'on',
                        "mpi": 'on',
                        "coverage": 'off',}
-    
+
     def requirements(self):
+
         if self.options.regressions == 'on':
             self.requires("fftw/3.3.9")
+
+        if self.options.docs == 'on' or self.options.examples == 'on':
+            # To prevent a conflict in the version of zlib required by libtiff and
+            # doxygen, override the version of zlib when either of them is required
+            self.requires("zlib/1.2.12", override=True)
 
         if self.options.examples == 'on' or self.options.regressions == 'on':
             self.requires("libtiff/4.0.9")
@@ -34,9 +40,10 @@ class SoptConan(ConanFile):
 
         if self.options.docs == 'on':
             self.requires("doxygen/1.9.2")
+
     def build(self):
       cmake = CMake(self)
-      
+
       cmake.definitions['regressions'] = self.options.regressions
       cmake.definitions['docs'] = self.options.docs
       cmake.definitions['examples'] = self.options.examples
@@ -49,6 +56,6 @@ class SoptConan(ConanFile):
 
       cmake.definitions['CMAKE_C_COMPILER_LAUNCHER'] = "ccache"
       cmake.definitions['CMAKE_CXX_COMPILER_LAUNCHER'] = "ccache"
-      cmake.definitions['CMAKE_VERBOSE_MAKEFILE:BOOL'] = "OFF"
+      cmake.definitions['CMAKE_VERBOSE_MAKEFILE:BOOL'] = "ON"
       cmake.configure()
       cmake.build()
