9823c8426562016173a09ca1599a86c6325afbfd
Sarah Jaffa <SJaffa@users.noreply.github.com>
2022-09-23 10:12:11 +0100

    Removed output image

--- cpp/tests/inpainting.cc
+++ cpp/tests/inpainting.cc
@@ -31,7 +31,6 @@ typedef sopt::Image<Scalar> Image;
 TEST_CASE("Inpainting"){
   extern std::unique_ptr<std::mt19937_64> mersenne;
   std::string const input = "cameraman256";
-  std::string const output = "none";
 
   Image const image = sopt::notinstalled::read_standard_tiff(input);
 
@@ -48,16 +47,9 @@ TEST_CASE("Inpainting"){
   auto const sigma = y0.stableNorm() / std::sqrt(y0.size()) * std::pow(10.0, -(snr / 20.0));
   auto const epsilon = std::sqrt(nmeasure + 2 * std::sqrt(y0.size())) * sigma;
 
-
   std::normal_distribution<> gaussian_dist(0, sigma);
   Vector y(y0.size());
   for (sopt::t_int i = 0; i < y0.size(); i++) y(i) = y0(i) + gaussian_dist(*mersenne);
-  // Write dirty imagte to file
-  if (output != "none") {
-    Vector const dirty = sampling.adjoint() * y;
-    sopt::utilities::write_tiff(Matrix::Map(dirty.data(), image.rows(), image.cols()),
-                                "dirty_" + output + ".tiff");
-  }
 
   sopt::t_real const gamma = 18;
   sopt::t_real const beta = sigma * sigma * 0.5;
@@ -80,6 +72,9 @@ TEST_CASE("Inpainting"){
 
   auto const diagnostic = fb();
 
+  CHECK(diagnostic.good);
+  CHECK(diagnostic.niters < 500);
+
   // compare input image to cleaned output image
   // calculate mean squared error sum_i ( ( x_true(i) - x_est(i) ) **2 ) 
   // check this is less than the number of pixels * 0.01
@@ -87,12 +82,4 @@ TEST_CASE("Inpainting"){
   auto mse = (image - diagnostic.x.array()).square().sum() / image.size();
   CAPTURE(mse);
   CHECK(mse < 0.01);
-
-  if (output != "none")
-    sopt::utilities::write_tiff(Matrix::Map(diagnostic.x.data(), image.rows(), image.cols()),
-                                output + ".tiff");
-  // diagnostic should tell us the function converged
-  // it also contains diagnostic.niters - the number of iterations, and cg_diagnostic - the
-  // diagnostic from the last call to the conjugate gradient.
-  if (not diagnostic.good) throw std::runtime_error("Did not converge!");
 }
