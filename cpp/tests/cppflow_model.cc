d917ba33ceb398cb8b358f41455578002e5adc32
S Jaffa <SJaffa@users.noreply.github.com>
2022-12-16 16:36:25 +0000

    Apply suggestions from code review
    Comments and tidying
    
    Co-authored-by: Tuomas Koskela <t.koskela@ucl.ac.uk>

--- cpp/tests/cppflow_model.cc
+++ cpp/tests/cppflow_model.cc
@@ -15,7 +15,6 @@
 #include <tools_for_tests/directories.h>
 #include <tools_for_tests/tiffwrappers.h>
 
-// \min_{x} ||\Psi^Tx||_1 \quad \mbox{s.t.} \quad ||y - Ax||_2 < \epsilon and x \geq 0
 
 typedef double Scalar;
 typedef sopt::Image<Scalar> Image;
@@ -36,14 +35,13 @@ TEST_CASE("Cppflow Model"){
   cppflow::model model(std::string(sopt::notinstalled::models_directory() + "/DnCNN/snr_15_model.pb/"));
 
   // Run model on image
+  // TODO: Automatically detect the string parameters, see issue #320 
   auto output_vector = model({{"serving_default_input0:0", input_tensor}}, {"StatefulPartitionedCall:0"});
 
   // Get values from output (has to be same type as the input to the model - float)
   auto output_tensor = output_vector[0].get_data<float>();
 
   auto output_image = sopt::cppflowutils::convert_tensor_to_image(output_tensor, image_rows, image_cols);
-
-  //sopt::utilities::write_tiff(output_image, "./cameraman_output.tiff");
 
   // compare input image to cleaned output image
   // calculate mean squared error sum_i ( ( x_true(i) - x_est(i) ) **2 ) 
