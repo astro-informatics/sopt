67ab84a822496ea135b5e8539c09b2d8d4980c78
Michael McLeod <michaelmcleod@ucl.ac.uk>
2024-06-21 14:28:58 +0100

    Update comments on non differentiable functions

--- cpp/sopt/g_proximal.h
+++ cpp/sopt/g_proximal.h
@@ -6,7 +6,8 @@
 #include <utility>
 #include "sopt/forward_backward.h"
 
-// Abstract base class providing the interface to the g_proximal function
+// Abstract base class providing the interface for non differentiable functions 
+// with a proximal operator.
 template <typename SCALAR> class NonDifferentiableFunc {
 
 public:
@@ -18,14 +19,18 @@ public:
 
   // A function that prints a log message
   virtual void log_message() const = 0;
+
   // Return a function representing the proximal operator for this function.
   // Function must be of type t_Proximal, that is
   // void proximal_operator(Vector, real, Vector)
   virtual t_Proximal proximal_operator() const = 0;
+
   // Returns the function itself
   virtual Real function(t_Vector const &x) const = 0;
+
   // Transforms input image to a different basis.
   // Return linear_transform_identity() if transform not necessary.
   virtual const t_LinearTransform &Psi() const = 0;
-}; // class GProximal
+  
+}; // class NonDifferentiableFunc
 #endif
