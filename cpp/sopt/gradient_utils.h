7e68ef680943d42a245dafff8c23528f987d5722
Christian Gutschow <chris.g@cern.ch>
2024-11-22 10:09:45 +0000

    add gradient helper

---
+++ cpp/sopt/gradient_utils.h
@@ -0,0 +1,34 @@
+#ifndef SOPT_GRADIENT_UTILS_H
+#define SOPT_GRADIENT_UTILS_H
+
+#include "sopt/types.h"
+#include "sopt/linear_transform.h"
+#include <memory>
+
+namespace sopt {
+
+//! A helper object that can hold the measurement operator
+//! and the target vector for a given iteration state
+template <typename T>
+class IterationState {
+  public:
+  IterationState() = delete;
+
+  IterationState(const T& target_vis,
+                 std::shared_ptr<sopt::LinearTransform<T>> phi)
+                 : _target(target_vis) {
+    _phi = phi;
+  }
+
+  const T& target() const { return _target; }
+
+  const sopt::LinearTransform<T>& phi() const { return *_phi; }
+
+  private:
+  const T _target;
+
+  std::shared_ptr<sopt::LinearTransform<T>> _phi;
+};
+
+} // namespace sopt
+#endif
